839b63cff23cca4c5df45db62ba27a785a424aac
Yogs253 <yogsdaundkar@yahoo.in>
2025-09-23 21:28:35 +0530

    Update AppLaunch.java
    import org.junit.jupiter.api.Test;

--- src/test/java/TestAppLaunch/AppLaunch.java
+++ src/test/java/TestAppLaunch/AppLaunch.java
@@ -2,6 +2,7 @@ package TestAppLaunch;
 
 
 import static org.testng.AssertJUnit.assertEquals;
+import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
